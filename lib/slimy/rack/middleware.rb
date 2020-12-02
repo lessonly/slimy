8f7b3d9dba788b5ebb0472f28d59bdf2b66c910a
Stephen Gregory <stephengregory@LSNLY-MBP0127.local>
2020-12-02 16:33:45 -0500

    Add in error handling

--- lib/slimy/rack/middleware.rb
+++ lib/slimy/rack/middleware.rb
@@ -8,10 +8,15 @@ module Slimy::Rack
     def call(env)
       context = Slimy::Context.new(deadline: 200)
       begin
-      response = @app.call(env)
+        response = @app.call(env)
+        context.result_error! if response[0] >= 500
+      rescue Exception => error
+        context.result_error!
+        raise error
       ensure
         context.finish
         report(context)
+        return response
       end
     end
 
